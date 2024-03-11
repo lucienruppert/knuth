3a4c8111e8a2045c427a5a02b90a08e75eb6a24a
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2024-03-11 22:38:26 +0100

    add basic template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'knuth';
+  public maximumItems: Array<number> = [...Array(10).keys()].map((x) => x + 1);
+  public selectedNumber: number = 0;
+  public numberOfOutputs: number | undefined;
+  public outputsArray: Array<number> = [];
+  private baseItemsGroup: Array<number> = [];
+
+  public onSelectChange(): void {
+    if (this.selectedNumber == 0) return;
+    this.baseItemsGroup = this.maximumItems.slice(0, this.selectedNumber);
+    this.numberOfOutputs = this.baseItemsGroup.reduce(
+      (previous, current) => previous! * current!
+    );
+    this.outputsArray = [...Array(this.numberOfOutputs).keys()].map(number => number + 1);
+  }
 }
